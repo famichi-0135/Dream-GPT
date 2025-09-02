a86c3b7de6650c03824042935b009ff3c2791209
famichi-0135 <tomop0513.maey@gmail.com>
2025-09-02 20:43:53 +0900

    認証機能追加

--- src/app/(dashboard)/dashboard/calender/page.tsx
+++ src/app/(dashboard)/dashboard/calender/page.tsx
@@ -6,7 +6,6 @@ export default function Page() {
       <h1 className="text-4xl font-bold text-gray-900">カレンダー</h1>
       <div className=" space-y-4 flex justify-start">
         <DIYCalender />
-        
       </div>
     </div>
   );
---
+++ src/app/protected/page.tsx
@@ -0,0 +1,22 @@
+import { redirect } from "next/navigation";
+
+import { LogoutButton } from "@/components/(supabase)/logout-button";
+import { createClient } from "@/lib/server";
+
+export default async function ProtectedPage() {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase.auth.getClaims();
+  if (error || !data?.claims) {
+    redirect("/auth/login");
+  }
+
+  return (
+    <div className="flex h-svh w-full items-centepr justify-center gap-2">
+      <p>
+        Hello <span>{data.claims.email}</span>
+      </p>
+      <LogoutButton />
+    </div>
+  );
+}
